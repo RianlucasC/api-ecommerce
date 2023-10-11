dd8f1035143b805a900ed3f903cb8b7c60d6deef
Rianlucas7 <rianlucas592@gmail.com>
2023-10-11 10:24:00 -0300

    feat: cors

--- src/server.js
+++ src/server.js
@@ -3,11 +3,14 @@ const express = require("express");
 const routes = require("./routes");
 const AppError = require("./utils/AppError");
 const connection = require("./database/connection");
+const cors = require("cors");
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
 
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
