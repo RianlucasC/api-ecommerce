71a2518e5c9c7b28182fb2c0e61391578c0bb79b
Rianlucas7 <rianlucas592@gmail.com>
2023-10-15 15:59:38 -0300

    fix: description in productSchema

--- src/database/models/Product.js
+++ src/database/models/Product.js
@@ -6,9 +6,9 @@ const productSchema = new mongoose.Schema({
         required: true
     },
     description: {
-        type: String,
+        type: [String],
         required: true
-    },
+    } ,
     price: {
         type: Number,
         required: true
